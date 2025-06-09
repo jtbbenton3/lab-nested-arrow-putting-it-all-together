c4e068bb0fe726bd139d2250a6396bd8c6e15c5e
Jack Benton <jtb.benton@gmail.com>
2025-06-09 14:43:19 -0400

    Module 1 Lab

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+  let isLocked = false;
+  
+
+
+  return (passwordAttempt) => {
+    if (isLocked) {
+      return `Account locked due to too many failed login attempts`;
+    }
+
+    attemptCount = attemptCount + 1;
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    }
+
+    if(attemptCount < 3) {
+      return `Login attempt ${attemptCount} failed`;
+    }
+
+    if (attemptCount >= 3) {
+      isLocked = true;
+      return `Account locked due to too many failed login attempts`;
+    }
+      
+  };
+}
 
 
 module.exports = {
